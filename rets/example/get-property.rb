f66f52739706e0c58b175081954b57dff0868c47
Travis1282 <tmail20@gmail.com>
2018-03-01 14:57:38 -0600

    date loop working with RETS connector... now need to resolve null return

--- rets/example/get-property.rb
+++ rets/example/get-property.rb
@@ -24,44 +24,44 @@ rescue => e
   exit!
 end
 
-# (Date.new(2012, 01, 01)..Date.new(2012, 01, 30)).each do |date|
-#   # Do stuff with date
-# end
 
-pp t = (Date.new(2018-02-20))
-pp t.strftime("%Y-%d-%m")
-
-property = client.find (:all), {
-  search_type: 'Property',
-  class: 'DE',
-  query: '(CIT=CHICAGO), (ST=CLSD), (CLOSEDDATE=2018-02-20)',
-  limit: 1
-}
-
-
-property.each do |child|
-
+(Date.new(2018,02,01)).upto(Date.new(2018, 02, 20)).each do |date|
   
-    sqft = child['ASF'].to_f
+   queryIter = '(CIT=CHICAGO), (ST=CLSD), (ClOSEDDATE='+(date.strftime("%Y-%d-%m")).to_s+')'
 
-  unless sqft == 0 
+  property = client.find (:all), {
+    search_type: 'Property',
+    class: 'DE',
+    # query: '(CIT=CHICAGO), (ST=CLSD), (CLOSEDDATE=2018-02-20)',
+    query: queryIter,
+    limit: 1
+  }
 
-    salesPrice = child['SP'].to_f
-    puts closedate = 'Close Date: '<< (child['CLOSEDDATE'])
-    puts ppsqft = 'Price Per Square Foot: '<< ((salesPrice/sqft).floor).to_s
+
+  property.each do |child|
+
+    
+      sqft = child['ASF'].to_f
+
+    unless sqft == 0 
+
+      salesPrice = child['SP'].to_f
+      puts closedate = 'Close Date: '<< (child['CLOSEDDATE'])
+      puts ppsqft = 'Price Per Square Foot: '<< ((salesPrice/sqft).floor).to_s
 
 
-    request_query =(child['HSN']<<' '<< child['CP'] <<' '<< child["STR"] <<' '<< child["STREETSUFFIX"] <<'city=Chicago&state=IL&zip='<< child["ZP"] << '&apikey=e3b6ccab4a5249abb5c37b6bbc6a3a8c&format=json&census=false&notStore=false&version=4.01')
+      request_query =(child['HSN']<<' '<< child['CP'] <<' '<< child["STR"] <<' '<< child["STREETSUFFIX"] <<'city=Chicago&state=IL&zip='<< child["ZP"] << '&apikey=e3b6ccab4a5249abb5c37b6bbc6a3a8c&format=json&census=false&notStore=false&version=4.01')
 
 
-   
-    request_uri = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress='
-    url = "#{request_uri}#{request_query}"
-    buffer = JSON.parse(open(url).read)
-   
-    p lat = 'lat: '<< buffer["OutputGeocodes"][0]["OutputGeocode"]["Latitude"]
-    p long = 'long: '<< buffer["OutputGeocodes"][0]["OutputGeocode"]["Longitude"]
+     
+      request_uri = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress='
+      url = "#{request_uri}#{request_query}"
+      buffer = JSON.parse(open(url).read)
+     
+      p lat = 'lat: '<< buffer["OutputGeocodes"][0]["OutputGeocode"]["Latitude"]
+      p long = 'long: '<< buffer["OutputGeocodes"][0]["OutputGeocode"]["Longitude"]
 
+    end
   end
 end
 
